fc244ef5271276166974f10edd3cbc8e2fb0fb55
Arkan <38447230+ArkanTesfay@users.noreply.github.com>
2019-03-28 02:32:13 -0700

    Update account.cpp

--- account.cpp
+++ account.cpp
@@ -1,15 +1,13 @@
-//
-//  Account.cpp
-//  BankAccount
-//
-//  Created by Arkan Tesfay Mekonen on 3/24/19.
-//  Copyright © 2019 Arkan Tesfay Mekonen. All rights reserved.
-//
+
 
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
+
+
+
+
 
 class Account
 {
@@ -37,15 +35,13 @@ public: Account(int AN,double B )
         return Balance;
     }
     
-    public: void Deposit(double amount)
+    
+    
+public: double withDraw(double amount)
     {
-        Balance+=amount;
-        
-    }
-public: void withDraw(double amount)
-    {
-        if (Balance >1 && amount<Balance) {
-            Balance -=amount;
+        double amountWithdrawed=amount;
+        if (Balance >1 && amountWithdrawed<Balance) {
+            Balance -=amountWithdrawed;
             
         }else{
             cout <<"ALERT!!!"<<endl;
@@ -53,6 +49,13 @@ public: void withDraw(double amount)
             
             
         }
+        return  amountWithdrawed;
+    }
+public: void Deposit(double amount)
+    {
+        Balance+=amount;
+        cout <<"You Deposited $"<<amount<<". Into You Checking Account"<<". Your current Balance is "<<getBalance()<<endl;
+        
     }
     public : void accountRecipt()
     {
@@ -63,3 +66,157 @@ public: void withDraw(double amount)
 
 
 };
+class CheckingAccount : Account
+{
+    public : double interestRate;
+    public :double minBalance;
+    public :double serviceCharge=5;
+    public :bool balanceBelowMin;
+    
+public: CheckingAccount(Account user) :Account(user.getAccountNumber(),user.getBalance())
+    {
+        setInterestRate(user);
+        
+    };
+    
+public: void setInterestRate(Account balance)
+    {
+        double IR=balance.getBalance();
+        interestRate=IR*0.002*1;
+    }
+public: double retireiveInterestRate()
+    {
+        return interestRate;
+    }
+public: void setMinimumBalance(Account balance)
+    {
+        if (balance.getBalance()<=25) {
+            minBalance=balance.getBalance();
+        }
+    }
+public: double retireveMinimumBalance()
+    {
+        return minBalance;
+    }
+public: void setServiceCharges(int serviceC)
+    {
+        serviceCharge=serviceC;
+    }
+public: double retireveServiceCharges()
+    {
+        return serviceCharge;
+    }
+public: void postInterest()
+    {
+        cout << interestRate<< endl;
+    }
+public: void verifyBalance(Account balance)
+    {
+        if (balance.getBalance()<minBalance) {
+            balanceBelowMin=true;
+            balance.getBalance()-serviceCharge;
+            cout <<"ALERT!!! Your Balance is below minimum"<<endl;
+        }else
+        {
+            balanceBelowMin=false;
+            cout <<"Your balance is Good ('_')"<<endl;
+        }
+    }
+public: void writeCheck(double amount,Account user)
+    {
+        double checkAmount=user.withDraw(amount);
+        cout<< "I Authorize the Payment of $"<<checkAmount<<endl;
+        cout << "Now your Balance is $"<< user.getBalance()<<endl;
+    }
+public: void printAccountInfo(Account user)
+    {
+        cout <<" As of Today your Account Balance is $"<<user.getBalance()<<".your service charges is -$"<< serviceCharge<<" Your Account Number is #"<<user.getAccountNumber()<<"Your interestRate is $"<<retireiveInterestRate()<<endl;
+    }
+
+};
+class SavingAccount : Account
+{
+    public : double SavinginterestRate;
+    public :double SavingminBalance;
+    public :double SavingserviceCharge=5;
+    public :bool SavingbalanceBelowMin;
+    
+public: SavingAccount(Account S): Account(S.getAccountNumber(),S.getBalance())
+    {
+        setInterestRateSaving(S);
+        
+    };
+    
+public: void setInterestRateSaving(Account balance)
+    {
+        double IR=balance.getBalance();
+        SavinginterestRate=IR*0.002*1;
+    }
+public: double retireiveInterestRateSaving()
+    {
+        return SavinginterestRate;
+    }
+public: void setMinimumBalance(Account balance)
+    {
+        if (balance.getBalance()<=25) {
+            SavingminBalance=balance.getBalance();
+        }
+    }
+public: double retireveMinimumBalance()
+    {
+        return SavingminBalance;
+    }
+public: void setServiceCharges(int serviceC)
+    {
+        SavingserviceCharge=serviceC;
+    }
+public: double retireveServiceCharges()
+    {
+        return SavingserviceCharge;
+    }
+public: void postInterest()
+    {
+        cout << SavinginterestRate<< endl;
+    }
+public: void verifyBalance(Account balance)
+    {
+        if (balance.getBalance()<SavingminBalance) {
+            SavingbalanceBelowMin=true;
+            balance.getBalance()-SavingserviceCharge;
+            cout <<"ALERT!!! Your Balance is below minimum"<<endl;
+        }else
+        {
+            SavingbalanceBelowMin=false;
+            cout <<"Your balance is Good ('_')"<<endl;
+        }
+    }
+public: void writeCheckForSaving(double amount,Account user)
+    {
+        double checkAmount=user.withDraw(amount);
+        cout<< "I Authorize the Payment of $"<<checkAmount<<endl;
+        cout << "Now your Balance is $"<< user.getBalance()<<endl;
+    }
+public: void printAccountInfo(Account user)
+    {
+        cout <<" As of Today your Account Balance is $"<<user.getBalance()<<".your service charges is -$"<< SavingserviceCharge<<" Your Account Number is #"<<user.getAccountNumber()<<"Your interestRate is $"<<retireiveInterestRateSaving()<<endl;
+    }
+    
+};
+
+    int main() {
+        // insert code here...
+        Account awet=Account(3454,1000);
+        awet.accountRecipt();
+        CheckingAccount awetCheckingAccount=CheckingAccount(awet);
+        SavingAccount awetSavingAccount=SavingAccount(awet);
+
+        awetCheckingAccount.printAccountInfo(awet);
+        awetCheckingAccount.writeCheck(100,awet);
+        awet.Deposit(2000);
+        
+        
+        
+        return 0;
+    }
+
+
